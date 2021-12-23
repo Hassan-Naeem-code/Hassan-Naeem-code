255ba1372c137e0fdacc54379ca89649d844d85e
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2021-12-23 22:53:03 +0500

    qualification section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,21 @@ skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
 /*==================== QUALIFICATION TABS ====================*/
-
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]');
+tabs.forEach((tab)=>{
+  tab.addEventListener('click',()=>{
+    const target = document.querySelector(tab.dataset.target);
+    tabContent.forEach((tabContent)=>{
+      tabContent.classList.remove('qualification__active')
+    });
+    target.classList.add('qualification__active');
+    tab.forEach((tab)=>{
+      tab.classList.remove('qualification__active');
+    });
+    tab.classList.add('qualification__active');
+  })
+})
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
